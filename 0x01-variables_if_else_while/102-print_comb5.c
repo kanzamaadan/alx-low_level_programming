da9aacc7402d1840c9e41376c00d58ef82721fad
Kanza <kenzamaadan@example.com>
2023-07-14 23:35:48 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,38 +6,38 @@
  */
 int main(void)
 {
-    int num1, num2, num3, num4;
+	int num1, num2, num3, num4;
 
-    for (num1 = 0; num1 <= 9; num1++)
-    {
-        for (num2 = 0; num2 <= 9; num2++)
-        {
-            for (num3 = 0; num3 <= 9; num3++)
-            {
-                for (num4 = 0; num4 <= 9; num4++)
-                {
-                    int first = num1 * 10 + num2;
-                    int second = num3 * 10 + num4;
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			for (num3 = 0; num3 <= 9; num3++)
+			{
+				for (num4 = 0; num4 <= 9; num4++)
+				{
+					int first = num1 * 10 + num2;
+					int second = num3 * 10 + num4;
 
-                    if (first < second)
-                    {
-                        putchar(num1 + '0');
-                        putchar(num2 + '0');
-                        putchar(' ');
-                        putchar(num3 + '0');
-                        putchar(num4 + '0');
+					if (first < second)
+					{
+						putchar(num1 + '0');
+						putchar(num2 + '0');
+						putchar(' ');
+						putchar(num3 + '0');
+						putchar(num4 + '0');
 
-                        if (num1 != 9 || num2 != 8 || num3 != 9 || num4 != 9)
-                        {
-                            putchar(',');
-                            putchar(' ');
-                        }
-                    }
-                }
-            }
-        }
-    }
+						if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
