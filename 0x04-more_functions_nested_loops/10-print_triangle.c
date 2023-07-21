a6292e7679db8c7ff898ed175deb496f9a1f0401
Kanza <kenzamaadan@example.com>
2023-07-21 00:43:41 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of '#' characters
+ *                  of a specified size.
+ *
+ * @size: The size of the triangle to print.
+ */
+void print_triangle(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i; j++)
+_putchar(' ');
+
+for (j = 0; j <= i; j++)
+_putchar('#');
+
+_putchar('\n');
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototypes */
 int _putchar(char c);
-void fizz_buzz(void);
+void print_triangle(int size);
 
 #endif /* MAIN_H */
