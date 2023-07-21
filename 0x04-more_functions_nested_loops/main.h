edc6f9df11a2f8b300f125b53b983fb45a02e80b
Kanza <kenzamaadan@example.com>
2023-07-21 15:22:48 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,27 @@
 #define MAIN_H
 
 int _putchar(char c);
-int main(void);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
 void print_diagonal(int n);
 
-#endif /* MAIN_H */
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print number(int n);
+
+#endif
