08064780f43b589ee370de21d6f926f32e005631
Kanza <kenzamaadan@example.com>
2023-07-21 00:33:11 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototypes */
 int _putchar(char c);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
 void fizz_buzz(void);
-int main(void);
-	
+
 #endif /* MAIN_H */
