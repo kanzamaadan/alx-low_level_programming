855e10debe158aaed58d06c32cca46c80cf0462b
Kanza <kenzamaadan@example.com>
2023-08-10 15:00:31 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-int multiply(char *num1, char *num2, char *result, int len1, int len2, int len_result);
-int is_valid_number(char *num);
+int is_number(char *s);
+void multiply(char *num1, char *num2);
+void print_result(int *result, int size);
 
 #endif /* MAIN_H */
+
