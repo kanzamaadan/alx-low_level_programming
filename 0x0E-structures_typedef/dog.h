7bb5bed0e55bdc7132d0b86c21f94f8c0bb4173f
Kanza <kenzamaadan@example.com>
2023-08-14 13:02:02 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for dog information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description:dog with its name, age, and owner.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
