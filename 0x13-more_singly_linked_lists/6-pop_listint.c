0895a736abfef356dedf19f68b797b10fb1fc4ca
kanzamaadan <kenzamaadan@gmail.com>
2023-08-30 03:37:17 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,7 +3,7 @@
 /**
  * pop_listint - Deletes the head node of a listint_t linked list.
  * @head: Pointer to a pointer to the beginning of the list.
- * 
+ *
  * Return: The head node's data (n), or 0 if the list is empty.
  */
 int pop_listint(listint_t **head)
