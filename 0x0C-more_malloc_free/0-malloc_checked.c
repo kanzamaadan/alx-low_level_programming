14ae2f8fe4a5a3c64a01c341e2ea6e58f392a16c
kanzamaadan <kenzamaadan@gmail.com>
2024-01-27 06:32:26 -0800

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,27 +5,16 @@
 
 /**
  * malloc_checked - allocates memory using malloc, exit(98) if it fails
- * @b: size of the memory block to be allocated
+ * @b: size of the memory result to be allocated
  *
- * Return: pointer to the address of the memory block
+ * Return: pointer to the address of the memory result
  */
-
 void *malloc_checked(unsigned int b)
 {
-/* Attempt to allocate memory using malloc*/
-int *result = (int *)malloc(sizeof(int) * b);
+void *result;
 
-/*Check if the allocation was successful*/
+result = malloc(b);
 if (result == NULL)
-{
-/* If malloc fails, exit the program with status 98*/
 exit(98);
-}
-
-/* Note: It's not necessary to free the memory here */
-/*since the goal is to return the allocated pointer.*/
-/* Freeing it here would result in a memory leak.*/
-
-/*Return the pointer to the allocated memory*/
 return (result);
 }
