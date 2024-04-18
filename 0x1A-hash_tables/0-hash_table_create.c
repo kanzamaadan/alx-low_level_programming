92d50462a490b429582b557321bd5a4521bddd38
kanza maadan <kenza maadan@gmail.com>
2024-04-17 18:15:37 -0700

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,9 +3,10 @@
 /**
  * hash_table_create - create a new hash table
  * @size: this is array's size
+ *
  * Return: in case of any error return NULL
  * otherwise ptr to new hash table
-*/
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *ht;
@@ -16,15 +17,17 @@ if (ht == NULL)
 {
 return (NULL);
 }
-/*assigns size passed to func to size field of ht*/
+/* assigns size passed to func to size field of ht */
 ht->size = size;
-/*memory is allocated for the array of hash nodes*/
+/* memory is allocated for the array of hash nodes */
 ht->array = malloc(sizeof(hash_node_t *) * size);
 if (ht->array == NULL)
 {
-return (NULL);  /*allocation failed*/
+return (NULL);  /* allocation failed */
 }
 for (i = 0; i < size; i++)
+{
 ht->array[i] = NULL;
-return (ht);  /*returns a pointer to the new hash table*/
+}
+return (ht);  /* returns a pointer to the new hash table */
 }
